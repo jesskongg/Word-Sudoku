e9e6e887bc74d050b2e5d7a408fd79738c8105e1
Jessica Kong <jka109@sfu.ca>
2019-02-07 14:01:38 -0800

    Create new main menu class and xml

---
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        mButton = (Button) findViewById(R.id.start_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+}
